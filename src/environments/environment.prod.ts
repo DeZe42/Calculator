dd0af40b4b291cf1397da191d773ea5bcdc6f47d
Administrator <dev@leadingsoft.eu>
2021-10-28 12:58:34 +0300

    commit-03

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBb0fvRKpsnqyaEBdLVDsrLzfFJNVP2HsU",
+    authDomain: "calculator-7d8a8.firebaseapp.com",
+    projectId: "calculator-7d8a8",
+    storageBucket: "calculator-7d8a8.appspot.com",
+    messagingSenderId: "353406957748",
+    appId: "1:353406957748:web:93c9ae9559f8b7b39cbfe1",
+    measurementId: "G-3HE5HMPEF4"
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBb0fvRKpsnqyaEBdLVDsrLzfFJNVP2HsU",
+    authDomain: "calculator-7d8a8.firebaseapp.com",
+    projectId: "calculator-7d8a8",
+    storageBucket: "calculator-7d8a8.appspot.com",
+    messagingSenderId: "353406957748",
+    appId: "1:353406957748:web:93c9ae9559f8b7b39cbfe1",
+    measurementId: "G-3HE5HMPEF4"
+  }
 };
 
 /*
